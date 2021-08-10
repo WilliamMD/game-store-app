58f124e4738a45b6ac5fdbaa537bcd0a25b8070d
kukuhnoviandra1 <kukuhkaka31@gmail.com>
2021-08-11 01:57:11 +0700

    kukuh3--Product udah bisa CRUD,showProduct muncul id , name,email si pemilik kalo udah dipesen

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -1,4 +1,65 @@
+const { User, Product } = require ('../models')
+
+
 class ProductController {
+    static async showProducts(req, res) {
+      try {
+        let product = await Product.findAll({
+        order: [["id", "ASC"]],
+          include: [{
+              model : User,
+              attributes: ["id","name","email"]
+            //   required: true,
+          }]
+        });
+  
+        res.status(200).json(product);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    }
+
+    static async addProducts(req, res) {
+        try {
+          const { name, desc, price, stock, expirate_day, weight, category, brand, condition,total_sold,rating,views,UserId } = req.body;
+          let product = await Product.create({
+            name, desc, price, stock, expirate_day, weight, category, brand, condition,total_sold,rating,views,UserId
+          });
+          res.status(201).json (product);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      }
+
+    static async deleteProducts(req, res) {
+    try {
+          const id = +req.params.id;
+          let result = await Product.destroy({
+            where: { id },
+          });
+          res.status(200).json(result)
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      }
+    
+    static async updateProducts(req, res) {
+    try {
+      const id = +req.params.id;
+      const { name, desc, price, stock, expirate_day, weight, category, brand, condition,total_sold,rating,views,UserId } = req.body;
+      let product = await Product.update(
+        {
+            name, desc, price, stock, expirate_day, weight, category, brand, condition,total_sold,rating,views,UserId
+        },
+        {
+          where: { id },
+        }
+      );
+      res.status(200).json (product);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
 
 }
 
