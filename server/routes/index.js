b72b9af70d146cd2b5f6b6417de436d47720d521
kukuhnoviandra1 <kukuhkaka31@gmail.com>
2021-08-11 00:54:13 +0700

    tests

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,14 +11,14 @@ const UsersRoutes = require('./users')
 // const LineItemsRoutes = require('./lineitems')
 // const OrdersRoutes = require('./orders')
 // const ProductImagesRoutes = require('./productimages')
-// const ProductsRoutes = require('./products')
+const ProductsRoutes = require('./products')
 // const ShoppingCartsRoutes = require('./shoppingcarts')
 
 router.use('/users', UsersRoutes)
 // router.use('/line_items', LineItemsRoutes)
 // router.use('/orders', OrdersRoutes)
 // router.use('/products_images', ProductImagesRoutes)
-// router.use('/products', ProductsRoutes)
+router.use('/products', ProductsRoutes)
 // router.use('/shopping_charts', ShoppingCartsRoutes)
 
 module.exports = router;
