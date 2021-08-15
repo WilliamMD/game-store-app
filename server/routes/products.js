53802d050190f40acc0ee8f2e57b16b472d7be20
kukuhnoviandra1 <kukuhkaka31@gmail.com>
2021-08-15 18:59:36 +0700

    KUKUH(16)--NEW UPDATE

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,13 +1,13 @@
 const ProductRouter = require('express').Router();
 const ProductController = require('../controllers/ProductController');
-const {authentication} = require('../middlewares/auth')
+const {authentication,authorization} = require('../middlewares/auth')
 
 
 ProductRouter.get('/',ProductController.showProducts)
 ProductRouter.get('/auth',authentication,ProductController.showProductsUsers)
 ProductRouter.get('/:id',ProductController.showProductsById)
 ProductRouter.post('/add',authentication,ProductController.addProducts)
-ProductRouter.delete('/delete/:id',authentication,ProductController.deleteProducts)
+ProductRouter.delete('/delete/:id',authentication,authorization,ProductController.deleteProducts)
 ProductRouter.put('/update/:id',authentication,ProductController.updateProducts)
 
 ProductRouter.put('/updateViews/:id',ProductController.updateViews)
